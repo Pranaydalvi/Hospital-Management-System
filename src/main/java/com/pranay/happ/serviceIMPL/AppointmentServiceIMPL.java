24d6e9886f76a4afb68e193d769495df920d248c
Pranay <pranaydalvi122@gmail.com>
2024-08-10 14:29:07 +0530

    appointmrnt Mail Sending Addeed

--- src/main/java/com/pranay/happ/serviceIMPL/AppointmentServiceIMPL.java
+++ src/main/java/com/pranay/happ/serviceIMPL/AppointmentServiceIMPL.java
@@ -1,6 +1,7 @@
 package com.pranay.happ.serviceIMPL;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.pranay.happ.dto.Response;
@@ -10,6 +11,7 @@ import com.pranay.happ.entity.UserRequest;
 import com.pranay.happ.repo.AppointmentRepository;
 import com.pranay.happ.repo.UserRepository;
 import com.pranay.happ.serviceI.AppointmentServiceI;
+import com.pranay.happ.util.EmailSender;
 import com.pranay.happ.util.UserRequestIDGenerator;
 
 @Service
@@ -20,36 +22,47 @@ public class AppointmentServiceIMPL implements AppointmentServiceI{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
-	public Response bookAppointment(Appointment appointment,String usernumber) {
-		Response response = new Response();
+	public Response bookAppointment(Appointment appointment, String usernumber) {
+Response response = new Response();
+        
+        // Find the user by usernumber
         UserRequest user = userRepository.findByUsernumber(usernumber);
+        
         if (user != null) {
-            if (appointment.getUserRequest() != null) {
-                UserRequest userRequest = appointment.getUserRequest();
-                if (user.getUsernumber().equals(userRequest.getUsernumber())) {
-                    String appointmentNumber = UserRequestIDGenerator.generateUserID();
-                    String apponum=UserRequestIDGenerator.generateUserID();
-                    appointment.setAppointmentNumber(apponum);
-                    appointment.setUserRequest(user);
-                    try {
-                        Appointment savedAppointment = appointmentRepository.save(appointment);
-                        if (savedAppointment != null) {
-                            response.setMsg("Appointment Data inserted.");
-                        } else {
-                            response.setMsg("Appointment Data not inserted.");
-                        }
-                    } catch (Exception e) {
-                        response.setMsg("Error occurred while inserting appointment data: " + e.getMessage());
-                    }
+            System.out.println("User found: " + user.getUsernumber());
+
+            // Associate the appointment with the found user
+            appointment.setUserRequest(user);
+
+            try {
+                // Generate a unique appointment number
+                String apponum = UserRequestIDGenerator.generateUserID();
+                appointment.setAppointmentNumber(apponum);
+
+                // Save the appointment
+                Appointment savedAppointment = appointmentRepository.save(appointment);
+
+                // Check if the appointment was saved successfully
+                if (savedAppointment != null) {
+                    // Send appointment confirmation email
+                    EmailSender.sendAppointmentConfirmationEmail(javaMailSender, savedAppointment);
+                    response.setMsg("Appointment Data inserted.");
                 } else {
-                    response.setMsg("User number mismatch.");
+                    response.setMsg("Appointment Data not inserted.");
                 }
+            } catch (Exception e) {
+                response.setMsg("Error occurred while inserting appointment data: " + e.getMessage());
             }
         } else {
             response.setMsg("User not found.");
         }
+
+        System.out.println("Response Message: " + response.getMsg());
         return response;
     }
-}
+	}
